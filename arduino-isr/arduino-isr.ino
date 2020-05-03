4f391fe0fa9fd39df605cd10dbdb5ff3705fc538
Alexandre Bolzon <blzn@mail.ru>
2020-05-03 01:07:24 -0300

    adjusts in syntax

--- arduino-isr/arduino-isr.ino
+++ arduino-isr/arduino-isr.ino
@@ -1,10 +1,14 @@
 const byte interruptPin = 2; // interrupt 0
 
 void setup() {
-  pinMode(interruptPin, INPUT PULLUP);
+  Serial.begin(9600);
+  pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), serialPrint, CHANGE);
 }
 
+void loop() {
+}
+
 void serialPrint() {
-  Serial.println(digitalRead(interruptPin));
+  Serial.print(digitalRead(interruptPin));
 }
